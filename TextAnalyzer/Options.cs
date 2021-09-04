4f7faea527d4d169738f8bd8b8f4531597e4495d
Gustav Reiz <gustav.reiz@logevo.se>
2021-09-05 00:25:18 +0200

    Adjusted help text for optional parameters

--- TextAnalyzer/Options.cs
+++ TextAnalyzer/Options.cs
@@ -8,10 +8,10 @@ namespace TextAnalyzer
         [Option('s', "source", Required = true, HelpText = "Specify the texts to analyze.", Min = 1, Max = 1000)]
         public IEnumerable<string> Sources { get; set; }
 
-        [Option('f', "frequency", Required = false, HelpText = "Specify the number of most frequent words that should be showed.")]
+        [Option('f', "frequency", Required = false, HelpText = "Specify the number of most frequent words that should be showed. Default is 20.")]
         public int CountForMostFrequentWords { get; set; } = 20;
 
-        [Option('l', "longest", Required = false, HelpText = "Specify the number of longest words that should be showed.")]
+        [Option('l', "longest", Required = false, HelpText = "Specify the number of longest words that should be showed. Default is 10.")]
         public int CountForLongestWords { get; set; } = 10;
     }
 }
